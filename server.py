04d81cdad5b7a4afbfa9a7fde5630541d90a1c6e
colinmeldrum <44164975+colinmeldrum@users.noreply.github.com>
2020-09-08 08:42:49 +0200

    Translation

---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+### The variables to display in the interface
+
+messageWelcome = "Future Python Developer"
+objective = "The objective of this project is to create an interface for a Python Web application."
+
+
+### The variables to display in the interface
+
+@app.route("/")
+def index():
+    return render_template("index.html", text = messageWelcome, message = objective )
+
+if __name__ == "__main__":
+    app.run(debug=True)
